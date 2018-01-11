61a1c283793df6b03fd5a30d81abadebf0d30914
AndrewAndersson <anderssona258@gmail.com>
2018-01-11 22:36:11 +0200

    add to task 4 (session17)

--- tasksSession_17/common.js
+++ tasksSession_17/common.js
@@ -38,3 +38,21 @@ function getExt(str) {
 console.log(getExt('/home/user/project/script.js'));
 
 //////////////////////////////////
+// 4. Напишите функцию, которая удаляет дубликаты из массива. Например, 
+// входной массив: [1, 2, 2, 4, 5, 4, 7, 8, 7, 3, 6], 
+// массив который возвращает функция [1, 2, 4, 5, 7, 8, 3, 6]
+
+let arr = [1, 2, 2, 4, 5, 4, 7, 8, 7, 3, 6];
+
+function filterArr(arr) {
+    let outArr = [];
+    let cach = '';
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i - 1] == arr[i]) continue;
+        cach += arr[i] + ',';
+    }
+    return () => outArr = cach.slice(0, -1).split(',');
+}
+
+let setArr = filterArr(arr);
+console.log(setArr());
