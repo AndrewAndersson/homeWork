f4f10c23551faeacf0f8c0a3a883840448210209
AndrewAndersson <anderssona258@gmail.com>
2018-01-22 00:15:04 +0200

    made changes session17

--- session17/js/main.js
+++ session17/js/main.js
@@ -1,39 +1,58 @@
 'use strict';
 var btn = document.getElementById("play");
 
-function transform() {
-    let newArr = [];
+function getSplice(arr) {
+    arr.splice(5, 1);
+}
 
-    function getSplice(arr) {
-        arr.splice(6, 1);
-        return arr;
-    }
-    getSplice(data);
-
-    data.forEach(function(item, index) {
-        newArr.push({
+function getNewArr(arr) {
+    data.forEach((item) => {
+        arr.push({
             url: item.url,
             name: item.name,
             params: item.params,
             description: item.description,
-            date: index.date
+            date: item.date
         });
     });
+}
 
-    data = newArr.map(function(item, index) {
+function getName(item) {
+    return `${item.name.charAt(0).toUpperCase()}${item.name.substr(1).toLowerCase()}`;
+}
+
+function getDate(item) {
+    return moment(item.date).format('YYYY/MM/DD, HH:mm');
+}
+
+function getData(arr) {
+    data = arr.map((item) => {
         return {
-            name: item.name.charAt(0) + item.name.substr(1).toLowerCase(),
+            name: getName(item),
             url: `http://${item.url}`,
-            description: item.description.substring(0, 15) + "...",
-            date: moment(item.date).format('YYYY/MM/DD, h:mm'),
+            description: `${item.description.substring(0, 15)}...`,
+            date: getDate(item),
             params: `${item.params.status}=>${item.params.progress}`,
             isVisible: item.params.status
         };
     });
+}
 
+function getFilter() {
     data = data.filter(item => item.isVisible == true);
-
     console.log(data);
+}
+
+function transform() {
+    let newArr = [];
+
+    getSplice(data);
+
+    getNewArr(newArr);
+
+    getData(newArr);
+
+    getFilter();
 }
 
 btn.addEventListener("click", transform);
